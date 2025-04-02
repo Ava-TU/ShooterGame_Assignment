ec84aa0b88c3827bbdb12d5ddecce939a1bc1c8d
Ava-TU <C24384981@myTUDublin.ie>
2025-04-02 18:22:13 +0100

    blocks moving and deleting
    blocks randomly spawn in, move towards player, and despawns after passing the camera

---
+++ shooter_assignment/Scripts/modules.gd
@@ -0,0 +1,11 @@
+extends Node3D
+
+@onready var level = $"../"
+var speed = 10
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position.z += speed * delta
+	if position.z > 25:
+		level.SpawnModule(position.z + (level.amount * level.offset))
+		queue_free()
