b45611969409cead9bcc85b740c228b3fe38d612
Ava-TU <C24384981@myTUDublin.ie>
2025-04-18 01:52:49 +0100

    bullets disappear after 3 seconds

--- shooter_assignment/Scenes/paint.gd
+++ shooter_assignment/Scenes/paint.gd
@@ -1,6 +1,9 @@
 extends CharacterBody3D
 
-var speed = 50
+var speed = 25
 
 func _process(delta):
 	position += transform.basis * Vector3(0, 0, -speed) * delta
+	
+	if position.z < -75:
+		queue_free()
