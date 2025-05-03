ad3400a8949fa7a237df7433fa9dc3ec9ee21862
Ava-TU <C24384981@myTUDublin.ie>
2025-05-03 18:29:51 +0100

    particles in

--- shooter_assignment/Scripts/painting_one.gd
+++ shooter_assignment/Scripts/painting_one.gd
@@ -1,8 +1,11 @@
 extends Node3D
 
+@onready var paintOneEmit = $CPUParticles3D
 
 func _process(delta: float) -> void:
 	print(global_position.y)
+	paintOneEmit.emitting = true
+	
 	if position.y < -5:
 		queue_free()
 		print("DeletedCanvas")
