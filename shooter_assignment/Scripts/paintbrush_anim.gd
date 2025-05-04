f76d0e9141f9197655a64a5c72bc6cd5d5d1c4e5
Ava-TU <C24384981@myTUDublin.ie>
2025-05-04 21:48:05 +0100

    Made paintings rotate around in game over scene

--- shooter_assignment/Scripts/paintbrush_anim.gd
+++ shooter_assignment/Scripts/paintbrush_anim.gd
@@ -4,7 +4,7 @@ extends AnimationPlayer
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	speed_scale = 0.5
-	#play("PaintbrushIdle")
+
 func _process(_delta: float) -> void:
 	if (Input.is_action_just_pressed("shoot")):
 		play("PaintbrushClick")
